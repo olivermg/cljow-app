1f8f5a0f533384bbb2f6f0df5a37b619af7373ec
Oliver Wegner <void1976@gmail.com>
2019-02-19 17:23:48 +0100

    started implementing core async messaging emitter & receiver

--- src/ow/app/messaging/emitter/core_async.clj
+++ src/ow/app/messaging/emitter/core_async.clj
@@ -1,10 +1,10 @@
-(ns ow.app.message-broker.core-async
+(ns ow.app.messaging.emitter.core-async
   (:require [clojure.core.async :as a]
             [clojure.tools.logging :as log]
             [ow.app.lifecycle :as owl]
             [ow.app.messaging.emitter :as owme]))
 
-(defrecord CoreAsyncEmitter [in out]
+(defrecord CoreAsyncEmitter [in out wait-timeout-ms]
 
   owl/Lifecycle
 
@@ -17,11 +17,29 @@
   owme/Emitter
 
   (emit [this type data]
-    )
+    (let [receipt {::error-chan    (a/promise-chan)
+                   ::response-chan (a/promise-chan)}
+          msg {::receipt receipt
+               ::data    data
+               ::type    type}]
+      (a/put! out msg)
+      receipt))
 
-  (wait [this receipt]
-    ))
+  (wait [this {:keys [::error-chan ::response-chan] :as receipt}]
+    (let [timeout-chan (a/timeout wait-timeout-ms)
+          [msg ch]     (a/alts!! [response-chan error-chan timeout-chan])]
+      (a/close! response-chan)
+      (a/close! error-chan)
+      (condp = ch
+        response-chan (::result msg)
+        error-chan    (let [e (::error msg)]
+                        (throw (if (instance? Throwable e)
+                                 e
+                                 (ex-info "ERROR" {:error (::error e)
+                                                   :receipt receipt}))))
+        timeout-chan  (throw (ex-info "TIMEOUT" {:receipt receipt}))))))
 
-(defn core-async-emitter [in out]
-  (map->CoreAsyncEmitter {:in  in
-                          :out out}))
+(defn core-async-emitter [in out & {:keys [wait-timeout-ms]}]
+  (map->CoreAsyncEmitter {:in in
+                          :out out
+                          :wait-timeout-ms (or wait-timeout-ms 10000)}))
--- src/ow/app/messaging/receiver/core_async.clj
+++ src/ow/app/messaging/receiver/core_async.clj
@@ -1,13 +1,32 @@
 (ns ow.app.messaging.receiver.core-async
-  (:require [ow.app.lifecycle :as owl]
+  (:require [clojure.core.async :as a]
+            [ow.app.lifecycle :as owl]
+            [ow.app.messaging.component :as owmc]
             [ow.app.messaging.receiver :as owmr]))
 
-(defrecord CoreAsyncReceiver [in out components]
+(defrecord CoreAsyncReceiver [in out components
+                              piped-in]
 
   owl/Lifecycle
 
   (start [this]
-    this)
+    (if-not piped-in
+      (let [piped-in (a/pipe in (a/chan))
+            pub (a/pub piped-in :ow.app.messaging/type)
+            xf (comp (map (fn [c]
+                            [(type c) (owmc/get-dispatch-map c)]))
+                     (map (fn [[t dm]]
+                            (into [] (map (fn [[k h]]
+                                            [t k h]))
+                                  dm)))
+                     (mapcat identity)
+                     (map (fn [x]
+                            (a/go-loop [msg (a/<! in)]
+                              
+                              (recur (a/<! in))))))]
+        (transduce xf (constantly nil) components)
+        (assoc this :piped-in piped-in))
+      this))
 
   (stop [this]
     this)
