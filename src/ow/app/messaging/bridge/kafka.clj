97df4a580ea9a655802bda31f245a2570c741da8
Oliver Wegner <void1976@gmail.com>
2019-03-06 14:06:21 +0100

    working on kafka implementation for bridge

--- src/ow/app/messaging/bridge/kafka.clj
+++ src/ow/app/messaging/bridge/kafka.clj
@@ -8,37 +8,39 @@
            [org.apache.kafka.clients.consumer ConsumerConfig KafkaConsumer ConsumerRecords]))
 
 (defrecord KafkaOutBridge [ch topic
-                           pipe producer]
+                           pipe]
 
   owl/Lifecycle
 
   (start [this]
     (if-not pipe
-      (let [pipe (a/chan)
+      (let [pipe (a/pipe ch (a/chan))
             kprops (doto (Properties.)  ;; TODO: parameterize config:
                      (.put ProducerConfig/CLIENT_ID_CONFIG "kafkaoutbridge1")
                      (.put ProducerConfig/BOOTSTRAP_SERVERS_CONFIG "kafka:9092")
                      (.put ProducerConfig/KEY_SERIALIZER_CLASS_CONFIG "org.apache.kafka.common.serialization.StringSerializer")
                      (.put ProducerConfig/VALUE_SERIALIZER_CLASS_CONFIG "org.apache.kafka.common.serialization.StringSerializer"))
             kproducer (KafkaProducer. kprops)]
-        (a/pipe ch pipe)
         (a/go-loop [msg (a/<! pipe)]
-          (let [topic (if (fn? topic)
-                        (topic msg)
-                        topic)
-                smsg (pr-str msg)  ;; TODO: use nippy for serialization
-                ]
-            (println "sending" smsg)
-            (.send kproducer (ProducerRecord. topic (hash smsg) smsg))  ;; TODO: does any other key make more sense?
-            (recur (a/<! pipe))))
-        (assoc this :pipe pipe :producer kproducer))
+          (if-not (nil? msg)
+            (do (println "msg in" msg)
+                (let [topic (if (fn? topic)
+                              (topic msg)
+                              topic)
+                      smsg (pr-str msg)  ;; TODO: use nippy for serialization
+                      ]
+                  (println "sending" smsg)
+                  (.send kproducer (ProducerRecord. topic (-> smsg hash str) smsg))  ;; TODO: does any other key make more sense?
+                  (recur (a/<! pipe))))
+            (do (.close kproducer)
+                (println "KafkaOutBridge stopped."))))
+        (assoc this :pipe pipe))
       this))
 
   (stop [this]
     (if pipe
-      (do (.close producer)
-          (a/close! pipe)
-          (assoc this :pipe nil :producer nil))
+      (do (a/close! pipe)
+          (assoc this :pipe nil))
       this)))
 
 (defn kafka-out-bridge [ch topic]
@@ -46,13 +48,13 @@
                         :topic topic}))
 
 
-(defrecord KafkaInBridge [ch topic
-                          consumer future]
+(defrecord KafkaInBridge [ch topic run?
+                          future*]
 
   owl/Lifecycle
 
   (start [this]
-    (if-not consumer
+    (if-not future*
       (let [kprops (doto (Properties.)
                      (.put ConsumerConfig/CLIENT_ID_CONFIG "kafkainbridge1")
                      (.put ConsumerConfig/GROUP_ID_CONFIG "kafkabridgegroup")
@@ -60,33 +62,28 @@
                      (.put ConsumerConfig/KEY_DESERIALIZER_CLASS_CONFIG "org.apache.kafka.common.serialization.StringDeserializer")
                      (.put ConsumerConfig/VALUE_DESERIALIZER_CLASS_CONFIG "org.apache.kafka.common.serialization.StringDeserializer"))
             kconsumer (KafkaConsumer. kprops)
-            _ (println :x1)
-            _ (.subscribe kconsumer (-> [topic] to-array Arrays/asList))
-            _ (println :x2)
+            _ (reset! run? true)
             fut (future
-                  (try
-                    (println :x3)
-                    (loop [recs (.poll kconsumer 500)]
-                      (println :x4)
-                      (doseq [rec recs]
-                        (let [dmsg (-> rec .value edn/read-string)]
-                          (a/put! ch dmsg)))
-                      (recur (.poll kconsumer 100)))
-                    (catch Exception e
-                      (println "EXCEPTION" e))
-                    (catch Error e
-                      (println "ERROR" e))))]
-        (println :x5)
-        (assoc this :consumer kconsumer :future fut))
+                  (.subscribe kconsumer [topic])
+                  (loop [recs (.poll kconsumer 100)]
+                    (doseq [rec recs]
+                      (let [dmsg (-> rec .value edn/read-string)]
+                        (println "recv msg" dmsg)
+                        (a/put! ch dmsg)))
+                    (if @run?
+                      (recur (.poll kconsumer 100))
+                      (do (.close kconsumer)
+                          (println "KafkaInBridge stopped.")))))]
+        (assoc this :future* fut))
       this))
 
   (stop [this]
-    (if consumer
-      (do (future-cancel future)
-          (.close consumer)
-          (assoc this :consumer nil :future nil))
+    (if future*
+      (do (reset! run? false)
+          (assoc this :future* nil))
       this)))
 
 (defn kafka-in-bridge [ch topic]
   (map->KafkaInBridge {:ch ch
-                       :topic topic}))
+                       :topic topic
+                       :run? (atom false)}))
