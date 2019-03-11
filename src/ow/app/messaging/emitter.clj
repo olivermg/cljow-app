6f9bca3cd3edc2064ad35c122811d6287025c7a6
Oliver Wegner <void1976@gmail.com>
2019-03-11 14:21:28 +0100

    restructuring messaging namespace; discarding obsolete modules

--- src/ow/app/messaging/emitter.clj
+++
@@ -1,9 +0,0 @@
-(ns ow.app.messaging.emitter)
-
-(defprotocol Emitter
-  (emit [this type data])
-  (recv [this receipt]))
-
-(defn emit-sync [this type data]
-  (->> (emit this type data)
-       (recv this)))
--- src/ow/app/messaging/receiver.clj
+++
@@ -1,3 +0,0 @@
-(ns ow.app.messaging.receiver)
-
-(defprotocol Receiver)
