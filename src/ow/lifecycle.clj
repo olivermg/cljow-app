408d294946b7c81244a948f1bf66f05cef4fc221
Oliver Wegner <void1976@gmail.com>
2019-03-23 18:33:00 +0100

    introducint ow.lifecycle

---
+++ src/ow/lifecycle.clj
@@ -0,0 +1,8 @@
+(ns ow.lifecycle
+  (:require [clojure.tools.logging :as log]))
+
+(defn construct [type data]
+  (assoc data ::type type))
+
+(defmulti start (fn [{:keys [::type] :as this} & args] type))
+(defmulti stop (fn [{:keys [::type] :as this} & args] type))
