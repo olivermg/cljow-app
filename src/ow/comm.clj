2416f07d4d99991ca6dd35b6ac470cfebd87ab6a
Oliver Wegner <void1976@gmail.com>
2019-03-25 18:33:38 +0100

    simplifying ow.comm:
    - no distinction between events & request-response

--- src/ow/comm.clj
+++ src/ow/comm.clj
@@ -1,15 +1,13 @@
-(ns ow.comm.request-response
+(ns ow.comm
   (:require [clojure.core.async :as a]
             [clojure.tools.logging :as log]
-            [ow.lifecycle :as owl]
-            [ow.comm.request-response.requester :as req]
-            [ow.comm.request-response.responder :as res]))
+            [ow.lifecycle :as owl]))
 
 (defn construct [in-ch handler]
-  (owl/construct ::requester-responder {:in-ch in-ch
-                                        :handler handler}))
+  (owl/construct ::comm {:in-ch in-ch
+                         :handler handler}))
 
-(defmethod owl/start ::requester-responder [{:keys [in-ch handler] :as this}]
+(defmethod owl/start ::comm [{:keys [in-ch handler] :as this}]
   (let [pipe (a/pipe in-ch (a/chan))]
     (a/go-loop [{:keys [request response-ch] :as request-map} (a/<! pipe)]
       (if-not (nil? request-map)
@@ -22,17 +20,25 @@
                                (catch Error e
                                  (log/debug "Handler threw Error" e)
                                  e))]
-                (a/put! response-ch response)))
+                (cond
+                  response-ch                    (a/put! response-ch response)
+                  (instance? Throwable response) (throw response)
+                  true                           response)))
             (recur (a/<! pipe)))
-        (log/info "Stopped requester-responder")))
+        (log/info "Stopped comm" (owl/get-type this))))
+    (log/info "Started comm" (owl/get-type this))
     (assoc this :pipe pipe)))
 
-(defmethod owl/stop ::requester-responder [{:keys [pipe] :as this}]
+(defmethod owl/stop ::comm [{:keys [pipe] :as this}]
   (when pipe
     (a/close! pipe))
   (assoc this :pipe nil))
 
-(defn request-async [out-ch request & {:keys [timeout]}]
+(defn emit [out-ch request]
+  (a/put! out-ch {:request request}))
+
+(defn request [out-ch request & {:keys [timeout]}]
+  (println "REQ" request)
   (let [response-ch (a/promise-chan)
         request-map {:request     request
                      :response-ch response-ch}
@@ -45,29 +51,26 @@
                             (ex-info "response channel was closed" {:request request}))
                           (ex-info "timeout while waiting for response" {:request request}))))]
     (a/put! out-ch request-map)
-    receipt))
-
-(defn wait-for-response [receipt]
-  (let [response (a/<!! receipt)]
-    (if-not (instance? Throwable response)
-      response
-      (throw response))))
-
-(defn request-sync [out-ch request & {:keys [timeout]}]
-  (->> (request-async out-ch request :timeout timeout)
-       (wait-for-response)))
+    (let [response (a/<!! receipt)]
+      (if-not (instance? Throwable response)
+        response
+        (throw response)))))
 
 
 
 #_(let [in     (a/chan)
       out    (a/chan)
-      c1     (-> (construct in (fn [this request]
-                                 (println "c1" this request)
-                                 (->> (assoc request :c1 :c1)
-                                      (request-sync out))))
+      c1     (-> (construct in (fn [this req]
+                                 (println "c1" req)
+                                 (->> (assoc req :c1 :c1)
+                                      (request out))))
                  owl/start)
-      c2     (-> (construct out (fn [this request]
-                                  (println "c2" this request)
-                                  (assoc request :c2 :c2)))
+      c2     (-> (construct out (fn [this req]
+                                  (println "c2" req)
+                                  (assoc req :c2 :c2)))
                  owl/start)]
-  (request-sync in {} :timeout 5000))
+  (doto (request in {} :timeout 5000)
+    (println "<-RESULT"))
+  (Thread/sleep 1000)
+  (owl/stop c2)
+  (owl/stop c1))
--- src/ow/comm/events.clj
+++
@@ -1,5 +0,0 @@
-(ns ow.comm.events)
-
-(defn new-event [topic data]
-  {::topic topic
-   ::data data})
