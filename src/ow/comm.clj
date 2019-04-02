ccd1cc975e4157abdcc25f7265470076fe3909d7
Oliver Wegner <void1976@gmail.com>
2019-04-02 12:17:11 +0200

    more logging

--- src/ow/comm.clj
+++ src/ow/comm.clj
@@ -11,22 +11,27 @@
   (if-not pipe
     (let [pipe (a/pipe in-ch (a/chan))]
       (a/go-loop [{:keys [request response-ch] :as request-map} (a/<! pipe)]
-        (log/trace "received request-map:" (select-keys request-map #{:id :flowid :topic}))
-        (if-not (nil? request-map)
-          (do (future
-                (let [response (try
-                                 (handler this request)
-                                 (catch Exception e
-                                   (log/debug "Handler threw Exception" e)
-                                   e)
-                                 (catch Error e
-                                   (log/debug "Handler threw Error" e)
-                                   e))]
-                  (cond
-                    response-ch                    (a/put! response-ch response)
-                    (instance? Throwable response) (throw response)
-                    true                           response)))
-              (recur (a/<! pipe)))))
+        (let [log-msg (select-keys request-map #{:id :flowid :topic})]
+          (log/trace "received request-map" log-msg)
+          (if-not (nil? request-map)
+            (do (future
+                  (let [response (try
+                                   (log/trace "invoking handler" log-msg)
+                                   (handler this request)
+                                   (catch Exception e
+                                     (log/debug "Handler threw Exception" e)
+                                     e)
+                                   (catch Error e
+                                     (log/debug "Handler threw Error" e)
+                                     e))]
+                    (cond
+                      response-ch                    (do (log/trace "sending back handler's response" log-msg)
+                                                         (a/put! response-ch response))
+                      (instance? Throwable response) (do (log/trace "throwing handler's exception" log-msg)
+                                                         (throw response))
+                      true                           (do (log/trace "discarding handler's response" log-msg)
+                                                         response))))
+                (recur (a/<! pipe))))))
       (assoc this ::pipe pipe))
     this))
 
@@ -40,7 +45,7 @@
                    :flowid  (get parent-event :flowid (rand-int Integer/MAX_VALUE))
                    :topic   topic
                    :request request}]
-    (log/trace "emitting event-map:" (select-keys event-map #{:id :flowid :topic}))
+    (log/trace "emitting event-map" (select-keys event-map #{:id :flowid :topic}))
     (a/put! out-ch event-map)))
 
 (defn request [out-ch request & {:keys [topic timeout parent-request]}]
@@ -50,6 +55,7 @@
                      :topic       topic
                      :request     request
                      :response-ch response-ch}
+        log-msg     (select-keys request-map #{:id :flowid :topic})
         receipt     (a/go
                       (let [timeout-ch    (a/timeout (or timeout 30000))
                             [response ch] (a/alts! [response-ch timeout-ch])]
@@ -58,9 +64,10 @@
                             response
                             (ex-info "response channel was closed" {:request request}))
                           (ex-info "timeout while waiting for response" {:request request}))))]
-    (log/trace "requesting request-map:" (select-keys request-map #{:id :flowid :topic}))
+    (log/trace "requesting request-map" log-msg)
     (a/put! out-ch request-map)
     (let [response (a/<!! receipt)]
+      (log/trace "received response" log-msg)
       (if-not (instance? Throwable response)
         response
         (throw response)))))
