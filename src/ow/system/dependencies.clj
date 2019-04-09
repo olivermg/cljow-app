328a1fd14a07a2ba7ef25f710a8b2fc0e63fac8a
Oliver Wegner <void1976@gmail.com>
2019-04-09 14:59:30 +0200

    improving dependency graph for system components

--- src/ow/system/dependencies.clj
+++ src/ow/system/dependencies.clj
@@ -1,5 +1,6 @@
 (ns ow.system.dependencies
-  (:require [clojure.tools.logging :as log]
+  (:require [clojure.set :as set]
+            [clojure.tools.logging :as log]
             [ow.system :as ows]
             [ubergraph.core :as ug]
             [ubergraph.alg :as uga]))
@@ -14,9 +15,13 @@
                      definition)]
     (when-not (-> dags uga/connect uga/dag?)
       (throw (ex-info "circular dependencies detected" {:dependency-graph (with-out-str (ug/pprint dags))})))
-    (let [dags-count (count (uga/connected-components dags))
-          roots      (take dags-count (uga/topsort dags))
-          start-order (->> roots
-                           (map #(uga/post-traverse dags %))
-                           (into #{}))]
-      (assoc system :start-order start-order))))
+    (let [dags-count         (count (uga/connected-components dags))
+          roots              (take dags-count (uga/topsort dags))
+          start-orders       (->> roots
+                                  (map #(uga/post-traverse dags %))
+                                  (into #{}))
+          missing-components (->> (set/difference (set (ug/nodes dags))
+                                                  (set (apply concat start-orders)))
+                                  (map vector))
+          start-orders       (apply conj start-orders missing-components)]
+      (assoc system :start-orders start-orders))))
