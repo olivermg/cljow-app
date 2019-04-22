08c15f3a92074843c9a613666dc58ecc420e97a2
Oliver Wegner <void1976@gmail.com>
2019-04-22 12:41:54 +0200

    playing around with joining-xf in ow.async

---
+++ src/ow/async.clj
@@ -0,0 +1,38 @@
+(ns ow.async
+  (:require [clojure.core.async :as a]))
+
+#_(defn )
+
+(def ch
+  (let [ts #{:a :b}
+        xf (fn [rf]
+             (let [t-map (volatile! {})]
+               (fn
+                 ([] (rf))
+                 ([result] (rf result))
+                 ([result {:keys [topic id] :as input}]
+                  (vswap! t-map update id #(assoc % topic input))
+                  (println "STATE" @t-map)
+                  (let [pending-msgs (get @t-map id)]
+                    (if (= (-> pending-msgs keys set) ts)
+                      (do (vswap! t-map dissoc id)
+                          (rf result input))))))))
+        ch (a/chan)
+        p  (a/pub ch :topic)
+        s1 (a/sub p :a (a/chan))
+        s2 (a/sub p :b (a/chan))
+        su (a/chan 1 xf)]
+    (a/pipe s1 su)
+    (a/pipe s2 su)
+    (a/go-loop [msg (a/<! su)]
+      (if-not (nil? msg)
+        (do (println "MSG" msg)
+            (recur (a/<! su)))
+        (println "QUIT")))
+    ch))
+
+(a/put! ch {:topic :a :id 11})
+(a/put! ch {:topic :b :id 11})
+(a/put! ch {:topic :a :id 22})
+(a/put! ch {:topic :b :id 22})
+(a/close! ch)
