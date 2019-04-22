8b53e558d373677327f0574c3be721c06451f35c
Oliver Wegner <void1976@gmail.com>
2019-04-22 12:45:47 +0200

    refactoring joining-xf in ow.async

--- src/ow/async.clj
+++ src/ow/async.clj
@@ -4,32 +4,33 @@
 #_(defn )
 
 (def ch
-  (let [ts #{:a :b}
-        xf (fn [rf]
-             (let [t-map (volatile! {})]
-               (fn
-                 ([] (rf))
-                 ([result] (rf result))
-                 ([result {:keys [topic id] :as input}]
-                  (vswap! t-map update id #(assoc % topic input))
-                  (println "STATE" @t-map)
-                  (let [pending-msgs (get @t-map id)]
-                    (if (= (-> pending-msgs keys set) ts)
-                      (do (vswap! t-map dissoc id)
-                          (rf result pending-msgs))))))))
-        ch (a/chan)
-        p  (a/pub ch :topic)
-        s1 (a/sub p :a (a/chan))
-        s2 (a/sub p :b (a/chan))
-        su (a/chan 1 xf)]
-    (a/pipe s1 su)
-    (a/pipe s2 su)
-    (a/go-loop [msg (a/<! su)]
-      (if-not (nil? msg)
-        (do (println "MSG" msg)
-            (recur (a/<! su)))
-        (println "QUIT")))
-    ch))
+  (letfn [(make-joining-xf [topics]
+            (let [topics (set topics)]
+              (fn [rf]
+                (let [t-map (volatile! {})]
+                  (fn
+                    ([] (rf))
+                    ([result] (rf result))
+                    ([result {:keys [topic id] :as input}]
+                     (vswap! t-map update id #(assoc % topic input))
+                     (println "STATE" @t-map)
+                     (let [pending-msgs (get @t-map id)]
+                       (if (= (-> pending-msgs keys set) topics)
+                         (do (vswap! t-map dissoc id)
+                             (rf result pending-msgs))))))))))]
+    (let [ch (a/chan)
+          p  (a/pub ch :topic)
+          s1 (a/sub p :a (a/chan))
+          s2 (a/sub p :b (a/chan))
+          su (a/chan 1 (make-joining-xf #{:a :b}))]
+      (a/pipe s1 su)
+      (a/pipe s2 su)
+      (a/go-loop [msg (a/<! su)]
+        (if-not (nil? msg)
+          (do (println "MSG" msg)
+              (recur (a/<! su)))
+          (println "QUIT")))
+      ch)))
 
 (a/put! ch {:topic :a :id 11})
 (a/put! ch {:topic :b :id 11})
