c25eb3eaa2fb4309ee6c1043485991a2c12b723e
Oliver Wegner <void1976@gmail.com>
2019-04-29 15:40:02 +0200

    working on oauth client

--- src/ow/oauth/client.clj
+++ src/ow/oauth/client.clj
@@ -12,6 +12,9 @@
     (oocts/set-token token-storage token-id oauth-token)
     oauth-token))
 
+(defn get-token [{:keys [token-storage] :as this} token-id]
+  (oocts/get-token token-storage token-id))  ;; TODO: don't return it when it's expired
+
 (defn request [{:keys [token-storage oauth-requester] :as this} token-id method path & {:keys [headers body]}]
   (letfn [(has-expired? [expires-at]
             false)
@@ -19,16 +22,16 @@
           (expires-soon? [expires-at]
             false)
 
-          (refresh [oauth-token]
-            (owclj/tries [5
+          (refresh [oauth-token]  ;; FIXME: implement protection for multiple overlapping refreshes
+            #_(owclj/tries [5
                           :try-sym       try
                           :exception-f   (fn [e try]
                                            (log/warn "refresh failed" try (str e)))
                           :retry-delay-f (owclj/make-retry-delay-log10-f :exp 1.5 :cap (* 60 1000) :varpct 10.0)]
-                         (log/trace "refresh, try" try)
-                         (let [oauth-token (oocr/refresh oauth-requester oauth-token)]
-                           (oocts/set-token token-storage token-id oauth-token)
-                           oauth-token)))
+                         (log/trace "refresh, try" try))
+            (let [oauth-token (oocr/refresh oauth-requester oauth-token)]
+              (oocts/set-token token-storage token-id oauth-token)
+              oauth-token))
 
           (refresh-async [oauth-token]
             (future
@@ -41,12 +44,12 @@
                 (let [oauth-token (refresh oauth-token)]
                   (oocr/request oauth-requester oauth-token method path headers body)))))]
 
-    (let [{:keys [expires-at] :as oauth-token} (oocts/get-token token-storage token-id)
-          oauth-token (or (cond
-                            (has-expired? expires-at)  (refresh oauth-token)
-                            (expires-soon? expires-at) (do (refresh-async oauth-token) nil))
-                          oauth-token)]
-      (request oauth-token))))
+    (if-let [{:keys [expires-at] :as oauth-token} (oocts/get-token token-storage token-id)]
+      (request (or (cond
+                     (has-expired? expires-at)  (refresh oauth-token)
+                     (expires-soon? expires-at) (do (refresh-async oauth-token) nil))
+                   oauth-token))
+      (throw (ex-info "no oauth-token found" {:token-id token-id})))))
 
 (defmethod ol/start* OAuthClient [this dependencies]
   (merge this dependencies))
