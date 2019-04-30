1dd2be07798663d00bdfcff16732d2b0546bdc67
Oliver Wegner <void1976@gmail.com>
2019-04-30 17:28:05 +0200

    start working on token-storage protection for simultaneous refreshes

--- src/ow/oauth/client/token_storage.clj
+++ src/ow/oauth/client/token_storage.clj
@@ -1,5 +1,9 @@
-(ns ow.oauth.client.token-storage)
+(ns ow.oauth.client.token-storage
+  (:refer-clojure :rename {update update-clj}))
 
 (defprotocol TokenStorage
   (get-token [this id])
   (set-token [this id token-data]))
+
+(defn update [this id token-data]
+  )
