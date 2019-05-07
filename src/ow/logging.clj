733815f7c88774fa200fe008130ec3d30d5fb7f7
Oliver Wegner <void1976@gmail.com>
2019-05-07 16:09:46 +0200

    replacing with-logged-step by defnl,
    including logging of namespace, function name, timestamp and args in
    stacktrace.

--- src/ow/logging.clj
+++ src/ow/logging.clj
@@ -5,9 +5,25 @@
 
 (def ^:dynamic +callinfo+ {:trace []})
 
-(defmacro with-logged-step [& body]
-  `(binding [+callinfo+ (update +callinfo+ :trace conj (rand-int 10000))]
+#_(defmacro with-logged-step [& body]
+  `(binding [+callinfo+ (update +callinfo+ :trace conj {:id   (rand-int 10000)
+                                                        ;;;:form ~(str &form)
+                                                        :ns   ~(str *ns*)
+                                                        :time (java.util.Date.)
+                                                        #_:fn   #_(-> (Thread/currentThread)
+                                                                  .getStackTrace
+                                                                  (nth 2)
+                                                                  str)})]
      ~@body))
+
+(defmacro defnl [name [& args] & body]
+  `(defn ~name [~@args]
+     (binding [+callinfo+ (update +callinfo+ :trace conj {:id   (rand-int 10000)
+                                                          ;;;:ns   ~(str *ns*)
+                                                          :time (java.util.Date.)
+                                                          :fn   ~(str *ns* "/" name)
+                                                          :args (list ~@args)})]
+       ~@body)))
 
 (defmacro with-logging-data [data & body]
   `(binding [+callinfo+ (update +callinfo+ :data merge ~data)]
@@ -15,50 +31,51 @@
 
 (defmacro log [& msgs]
   `(log/info (pr-str (assoc +callinfo+
-                            :msg (s/join " " (list ~@msgs))
-                            :date (java.util.Date.)
-                            :ns ~(str *ns*)))))
+                            :msg  (s/join " " (list ~@msgs))
+                            ;;;:form ~(str &form)
+                            :time (java.util.Date.)
+                            :ns   ~(str *ns*)))))
 
 
-#_(do (defn foo1 [x]
+#_(do (defnl foo1 [x]
       (log "foo1 1" x)
       (Thread/sleep (rand-int 1000))
       (log "foo1 2" x)
       (inc x))
 
-    (defn foo2 [x]
+    (defnl foo2 [x]
       (log "foo2 1" x)
       (Thread/sleep (rand-int 1000))
       (log "foo2 2" x)
       (inc x))
 
-    (defn bar1 [x]
+    (defnl bar1 [x]
       (log "bar1 1" x)
-      (let [r (doall (pvalues (with-logged-step
-                                (foo1 (inc x)))
-                              (with-logged-step
-                                (foo2 (inc x)))))]
+      (let [r (doall (pvalues #_(with-logged-step)
+                              (foo1 (inc x))
+                              #_(with-logged-step)
+                              (foo2 (inc x))))]
         (log "bar1 2" x)
         r) )
 
-    (defn bar2 [x]
+    (defnl bar2 [x]
       (log "bar2 1" x)
-      (let [r (doall (pvalues (with-logged-step
-                                (foo1 (inc x)))
-                              (with-logged-step
-                                (foo2 (inc x)))))]
+      (let [r (doall (pvalues #_(with-logged-step)
+                              (foo1 (inc x))
+                              #_(with-logged-step)
+                              (foo2 (inc x))))]
         (log "bar2 2" x)
         r))
 
-    (defn baz [x]
+    (defnl baz [x]
       (log "baz 1" x)
       (with-logging-data {:user "user123"}
-        (pvalues (with-logged-step
-                   (bar1 (inc x)))
+        (pvalues #_(with-logged-step)
+                 (bar1 (inc x))
                  (do (Thread/sleep 2000)
-                     (with-logged-step
-                       (bar2 (inc x))))))
+                     #_(with-logged-step)
+                     (bar2 (inc x)))))
       (log "baz 2" x))
 
-    (with-logged-step
-      (baz 123)))
+    #_(with-logged-step)
+    #_(baz 123))
