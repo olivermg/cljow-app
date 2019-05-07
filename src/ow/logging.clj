d3774422c5681e0a1481033ab8e679a5a51a245d
Oliver Wegner <void1976@gmail.com>
2019-05-07 14:46:23 +0200

    adding sample invocation to logging.clj

--- src/ow/logging.clj
+++ src/ow/logging.clj
@@ -20,42 +20,45 @@
                             :ns ~(str *ns*)))))
 
 
-(defn foo1 [x]
-  (log "foo1 1" x)
-  (Thread/sleep (rand-int 1000))
-  (log "foo1 2" x)
-  (inc x))
+#_(do (defn foo1 [x]
+      (log "foo1 1" x)
+      (Thread/sleep (rand-int 1000))
+      (log "foo1 2" x)
+      (inc x))
 
-(defn foo2 [x]
-  (log "foo2 1" x)
-  (Thread/sleep (rand-int 1000))
-  (log "foo2 2" x)
-  (inc x))
+    (defn foo2 [x]
+      (log "foo2 1" x)
+      (Thread/sleep (rand-int 1000))
+      (log "foo2 2" x)
+      (inc x))
 
-(defn bar1 [x]
-  (log "bar1 1" x)
-  (let [r (doall (pvalues (with-logged-step
-                            (foo1 (inc x)))
-                          (with-logged-step
-                            (foo2 (inc x)))))]
-    (log "bar1 2" x)
-    r) )
+    (defn bar1 [x]
+      (log "bar1 1" x)
+      (let [r (doall (pvalues (with-logged-step
+                                (foo1 (inc x)))
+                              (with-logged-step
+                                (foo2 (inc x)))))]
+        (log "bar1 2" x)
+        r) )
 
-(defn bar2 [x]
-  (log "bar2 1" x)
-  (let [r (doall (pvalues (with-logged-step
-                            (foo1 (inc x)))
-                          (with-logged-step
-                            (foo2 (inc x)))))]
-    (log "bar2 2" x)
-    r))
+    (defn bar2 [x]
+      (log "bar2 1" x)
+      (let [r (doall (pvalues (with-logged-step
+                                (foo1 (inc x)))
+                              (with-logged-step
+                                (foo2 (inc x)))))]
+        (log "bar2 2" x)
+        r))
 
-(defn baz [x]
-  (log "baz 1" x)
-  (with-logging-data {:user "user123"}
-    (pvalues (with-logged-step
-               (bar1 (inc x)))
-             (do (Thread/sleep 2000)
-                 (with-logged-step
-                   (bar2 (inc x))))))
-  (log "baz 2" x))
+    (defn baz [x]
+      (log "baz 1" x)
+      (with-logging-data {:user "user123"}
+        (pvalues (with-logged-step
+                   (bar1 (inc x)))
+                 (do (Thread/sleep 2000)
+                     (with-logged-step
+                       (bar2 (inc x))))))
+      (log "baz 2" x))
+
+    (with-logged-step
+      (baz 123)))
