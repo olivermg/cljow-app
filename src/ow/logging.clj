55c87ebd547700d4f7a387ec275ead3b58528a6b
Oliver Wegner <void1976@gmail.com>
2019-05-08 16:07:42 +0200

    working on ow.logging

--- src/ow/logging.clj
+++ src/ow/logging.clj
@@ -41,22 +41,27 @@
      ~@body))
 
 
-(defmacro log [level name & [msg data]]
+(defmacro log-data [name & [msg data]]
   (let [datasym (gensym (str name "-data-"))]
     `(let [~datasym ~data]
-       (log/log ~level (-> +callinfo+
-                           (assoc :name ~(str name)
-                                  :time (java.util.Date.)
-                                  :ns   ~(str *ns*))
-                           ~(if msg
-                              `(assoc :msg ~msg)
-                              `identity)
-                           ~(if data
-                              `(update :data merge (if (map? ~datasym)
-                                                     ~datasym
-                                                     {~(keyword datasym) ~datasym}))
-                              `identity)
-                           pr-str)))))
+       (-> +callinfo+
+           (assoc :name ~(str name)
+                  :time (java.util.Date.)
+                  :ns   ~(str *ns*))
+           ~(if msg
+              `(assoc :msg ~msg)
+              `identity)
+           ~(if data
+              `(update :data merge (if (map? ~datasym)
+                                     ~datasym
+                                     {~(keyword datasym) ~datasym}))
+              `identity)))))
+
+(defmacro log-str [name & [msg data]]
+  `(pr-str (log-data ~name ~msg ~data)))
+
+(defmacro log [level name & [msg data]]
+  `(log/log ~level (log-str ~name ~msg ~data)))
 
 (defmacro trace [name & [msg data]]
   `(log :trace ~name ~msg ~data))
@@ -78,37 +83,55 @@
 
 
 
-#_(do (defn foo1 [x]
-      (warn foo11 "foo1 1" x)
-      (Thread/sleep (rand-int 1000))
-      (info foo12 "foo1 2" x)
-      (inc x))
+#_(do (require '[clojure.core.async :as a])
 
-    (defn foo2 [x]
-      (warn foo21 "foo2 1" x)
-      (Thread/sleep (rand-int 1000))
-      (info foo22 "foo2 2" x)
-      (inc x))
+    (let [foo1r (a/chan)
+          foo1a (a/chan)
+          foo2r (a/chan)
+          foo2a (a/chan)]
 
-    (defn bar1 [x]
-      (warn bar11 "bar1 1" x)
-      (let [r (doall (pvalues (foo1 (inc x))
-                              (foo2 (inc x))))]
-        (info bar12 "bar1 2" x)
-        r) )
+      (a/go-loop [x (a/<! foo1r)]
+        (when-not (nil? x)
+          (warn foo11 "foo1 1" x)
+          (Thread/sleep (rand-int 1000))
+          (info foo12 "foo1 2" x)
+          (a/put! foo1a (inc x))))
 
-    (defn bar2 [x]
-      (warn bar21 "bar2 1" x)
-      (let [r (doall (pvalues (foo1 (inc x))
-                              (foo2 (inc x))))]
-        (info bar22 "bar2 2" x)
-        r))
+      (a/go-loop [x (a/<! foo2r)]
+        (when-not (nil? x)
+          (warn foo21 "foo2 1" x)
+          (Thread/sleep (rand-int 1000))
+          (info foo22 "foo2 2" x)
+          (a/put! foo2a (inc x))))
 
-    (defn baz [x]
-      (warn baz1 "baz 1" x)
-      (with-trace-data {:user "user123"}
-        (pvalues (bar1 (inc x))
-                 (bar2 (inc x))))
-      (info baz2 "baz 2" x))
+      (defn bar1 [x]
+        (warn bar11 "bar1 1" x)
+        (let [r (doall (pvalues (do (a/put! foo1r (inc x))
+                                    (a/<!! foo1a))
+                                (do (a/put! foo2r (inc x))
+                                    (a/<!! foo2a))))]
+          (info bar12 "bar1 2" x)
+          r))
 
-    (baz 123))
+      (defn bar2 [x]
+        (warn bar21 "bar2 1" x)
+        (let [r (doall (pvalues (do (a/put! foo1r (inc x))
+                                    (a/<!! foo1a))
+                                (do (a/put! foo2r (inc x))
+                                    (a/<!! foo2a))))]
+          (info bar22 "bar2 2" x)
+          r))
+
+      (defn baz [x]
+        (warn baz1 "baz 1" x)
+        (with-trace-data {:user "user123"}
+          (pvalues (bar1 (inc x))
+                   (bar2 (inc x))))
+        (info baz2 "baz 2" x))
+
+      (baz 123)
+      (Thread/sleep 3000)
+      (a/close! foo2a)
+      (a/close! foo2r)
+      (a/close! foo1a)
+      (a/close! foo1r)))
