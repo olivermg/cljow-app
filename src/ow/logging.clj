ba1ef3b07c183db3b4b2e6ecffa83f544bbcab72
Oliver Wegner <void1976@gmail.com>
2019-05-08 12:10:52 +0200

    working on logging.clj

--- src/ow/logging.clj
+++ src/ow/logging.clj
@@ -5,27 +5,27 @@
 
 (def ^:dynamic +callinfo+ {:trace []})
 
-#_(defmacro with-logged-step [& body]
-  `(binding [+callinfo+ (update +callinfo+ :trace conj {:id   (rand-int 10000)
-                                                        ;;;:form ~(str &form)
-                                                        :ns   ~(str *ns*)
-                                                        :time (java.util.Date.)
-                                                        #_:fn   #_(-> (Thread/currentThread)
-                                                                  .getStackTrace
-                                                                  (nth 2)
-                                                                  str)})]
+(defmacro make-trace-info* [name & args]
+  `(into {:id   (rand-int 10000)
+          :time (java.util.Date.)
+          :fn   ~(str *ns* "/" name)}
+         [~(when-not (empty? args)
+             `[:args (list ~@args)])]))
+
+(defmacro with-trace* [name [& args] & body]
+  `(binding [+callinfo+ (update +callinfo+ :trace conj (make-trace-info* ~name ~@args))]
      ~@body))
 
-(defmacro defnl [name [& args] & body]
+(defmacro with-trace [name & body]
+  `(with-trace* ~name []
+     ~@body))
+
+(defmacro defn-traced [name [& args] & body]
   `(defn ~name [~@args]
-     (binding [+callinfo+ (update +callinfo+ :trace conj {:id   (rand-int 10000)
-                                                          ;;;:ns   ~(str *ns*)
-                                                          :time (java.util.Date.)
-                                                          :fn   ~(str *ns* "/" name)
-                                                          :args (list ~@args)})]
+     (with-trace* ~name [~@args]
        ~@body)))
 
-(defmacro with-logging-data [data & body]
+(defmacro with-trace-data [data & body]
   `(binding [+callinfo+ (update +callinfo+ :data merge ~data)]
      ~@body))
 
@@ -37,19 +37,19 @@
                             :ns   ~(str *ns*)))))
 
 
-#_(do (defnl foo1 [x]
+#_(do (defn-traced foo1 [x]
       (log "foo1 1" x)
       (Thread/sleep (rand-int 1000))
       (log "foo1 2" x)
       (inc x))
 
-    (defnl foo2 [x]
+    (defn-traced foo2 [x]
       (log "foo2 1" x)
       (Thread/sleep (rand-int 1000))
       (log "foo2 2" x)
       (inc x))
 
-    (defnl bar1 [x]
+    (defn-traced bar1 [x]
       (log "bar1 1" x)
       (let [r (doall (pvalues #_(with-logged-step)
                               (foo1 (inc x))
@@ -58,7 +58,7 @@
         (log "bar1 2" x)
         r) )
 
-    (defnl bar2 [x]
+    (defn-traced bar2 [x]
       (log "bar2 1" x)
       (let [r (doall (pvalues #_(with-logged-step)
                               (foo1 (inc x))
@@ -67,9 +67,9 @@
         (log "bar2 2" x)
         r))
 
-    (defnl baz [x]
+    (defn-traced baz [x]
       (log "baz 1" x)
-      (with-logging-data {:user "user123"}
+      (with-trace-data {:user "user123"}
         (pvalues #_(with-logged-step)
                  (bar1 (inc x))
                  (do (Thread/sleep 2000)
@@ -78,4 +78,4 @@
       (log "baz 2" x))
 
     #_(with-logged-step)
-    #_(baz 123))
+    (baz 123))
