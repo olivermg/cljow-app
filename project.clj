81acda73a872fcb0e5f3f6f7b8ad0fa93ed0dc29
Oliver Wegner <void1976@gmail.com>
2019-06-01 14:50:29 +0200

    adding :pedantic? :abort

--- project.clj
+++ project.clj
@@ -23,4 +23,6 @@
                  [trptcolin/versioneer "0.2.0"]
                  #_[ubergraph "0.5.2"]]
 
-  :profiles {:dev {:dependencies [[org.apache.logging.log4j/log4j-core "2.11.2"]]}})
+  :profiles {:dev {:dependencies [[org.apache.logging.log4j/log4j-core "2.11.2"]]}}
+
+  :pedantic? :abort)
